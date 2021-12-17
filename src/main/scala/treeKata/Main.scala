bc1225041ce87c3df3639ff2e35d5327b63b1d14
Jared <jchud13@gmail.com>
2021-12-16 21:43:37 -0500

    Revised InorderTraversal to pass tests

--- src/main/scala/treeKata/Main.scala
+++ src/main/scala/treeKata/Main.scala
@@ -12,12 +12,11 @@ class BalancedBinaryTree(){
 	def InorderTraversal(tree: MyNode): ArrayBuffer[Int] = {
 		val retList = new ArrayBuffer[Int]()
 		if(tree.left != null){
-			val temp = InorderTraversal(tree.left)
-			retList.insertAll(0, temp)
+			retList ++=  InorderTraversal(tree.left)
 		}
 		retList += tree.heldValue
 		if(tree.right != null){
-			retList.insertAll(0, InorderTraversal(tree.right))
+			retList ++= InorderTraversal(tree.right)
 		}
 		return retList
 	}
